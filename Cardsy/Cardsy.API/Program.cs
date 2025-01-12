2205d17c77f6a1b8d100b166381a1833358f2413
TheOneHoff <hoffmania789@gmail.com>
2025-01-12 12:21:42 +0800

    setup serilog, openapi and scalar

--- Cardsy/Cardsy.API/Program.cs
+++ Cardsy/Cardsy.API/Program.cs
@@ -1,20 +1,63 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using Serilog;
 
-var builder = WebApplication.CreateSlimBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
 
 builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetConnectionString("Cache"));
 
+builder.Services.AddOpenApi(options =>
+{
+    if (builder.Environment.IsDevelopment())
+    {
+        options.AddDocumentTransformer((document, context, cancellationToken) =>
+        {
+                var development_host = "localhost";
+                document.Servers.Clear();
+                var http_port = builder.Configuration.GetValue<int>("ASPNETCORE_HTTP_PORTS");
+                document.Servers.Add(new()
+                {
+                    Url = $"http://{development_host}:{http_port}"
+                });
+                var https_port = builder.Configuration.GetValue<int?>("ASPNETCORE_HTTPS_PORTS");
+                if (https_port.HasValue)
+                {
+                    document.Servers.Add(new()
+                    {
+                        Url = $"https://{development_host}:{https_port}"
+                    });
+                }
+            return Task.CompletedTask;
+        });
+    }
+});
+
 var app = builder.Build();
+
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+app.UseHttpsRedirection();
+
 
 var sampleTodos = new Todo[] {
     new(1, "Walk the dog"),
